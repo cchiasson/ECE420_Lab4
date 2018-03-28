9c4c52a8f642589a056140940cf56bdcf70e44e5
cchiasson <chiasson@ualberta.ca>
2018-03-27 18:08:45 -0600

    Update main.c

--- randi/main.c
+++ randi/main.c
@@ -42,18 +42,12 @@ int main (int argc, char* argv[]){
     r_pre = malloc(nodecount * sizeof(double));
     for ( i = 0; i < nodecount; ++i) {
         r[i] = 1.0 / nodecount;
-	sub_r[i]=1.0/nodecount;
-    }
+	}
     GET_TIME(start);
     // Calculate the result
     
-    printf("before if %d\n", my_id);
-
-
     if (node_init(&nodehead, num_in_links, num_out_links, 0, nodecount)) return 254;
-    printf("before scatter %d\n", my_id);
     MPI_Scatter(r, sub_node, MPI_DOUBLE, sub_r, sub_node, MPI_DOUBLE, 0, MPI_COMM_WORLD);
-    printf("after scatter %d\n", my_id);
 
 
     damp_const = (1.0 - DAMPING_FACTOR) / nodecount;
@@ -61,23 +55,18 @@ int main (int argc, char* argv[]){
     do{
         ++iterationcount;
         // make a copy of the vector.
-        //vec_cp(r, r_pre, nodecount);
-	vec_cp(sub_r,sub_r_pre,sub_node);
+		vec_cp(sub_r,sub_r_pre,sub_node);
 
         for ( i = 0; i < sub_node; ++i){
             sub_r[i] = 0;
-            for ( j = 0; j < nodehead[i].num_in_links; ++j)
+            for ( j = 0; j < nodehead[i].num_in_links; ++j) {
                 sub_r[i] += sub_r_pre[nodehead[i].inlinks[j]] / num_out_links[nodehead[i].inlinks[j]];
+            }
             sub_r[i] *= DAMPING_FACTOR;
-            sub_r[i] += damp_const;
+            sub_r[i] += damp_const;     
+		}
 
-	    /*MPI_Comm_rank(MPI_COMM_WORLD, &my_id);
-	    if (my_id==1) {
-		printf("%f\n",sub_r[i]);
-	    }*/        
-	}
-
-	//printf("Before gather\n");
+	//~ printf("iteration: %d proc: %d\n", iterationcount, my_id);
 	//MPI_Comm_rank(MPI_COMM_WORLD, &my_id);
 	//printf("ID: %d\n",my_id);
         /*if (my_id==0) {
@@ -87,47 +76,39 @@ int main (int argc, char* argv[]){
 	else {
         	MPI_Gather(sub_r, sub_node, MPI_DOUBLE, NULL, 0, MPI_DOUBLE, 0, MPI_COMM_WORLD);
 	}*/
-	//printf("after gather\n");
+		if (rel_error(sub_r, sub_r_pre, sub_node)<EPSILON) {
+			printf("break condition %f\n", rel_error(sub_r, sub_r_pre, sub_node));
+		}
     } while(rel_error(sub_r, sub_r_pre, sub_node) >= EPSILON);
-    	printf("Program converges at %d th iteration for rank %d\n", iterationcount, my_id);
+    
+    
+    printf("Program converges at %d th iteration for rank %d\n", iterationcount, my_id);
 
     
     if (my_id==0) {
-	printf("attempting gather for 0\n");
        	MPI_Gather(sub_r, sub_node, MPI_DOUBLE, r, sub_node, MPI_DOUBLE, 0, MPI_COMM_WORLD);
     }
     
     else {
-	printf("attempting gather for others\n");
        	MPI_Gather(sub_r, sub_node, MPI_DOUBLE, NULL, 0, MPI_DOUBLE, 0, MPI_COMM_WORLD);
     }
     GET_TIME(end);
     time = end-start;
     
-	printf("attempting finalize\n");
     MPI_Finalize();
     
-	printf("after finalize\n");
 
 
-    //if (my_id == 0) {
+    if (my_id == 0) {
         Lab4_saveoutput(r, nodecount, time);
-    //}
+    }
     // post processing
-    printf("destroying\n");
     node_destroy(nodehead, nodecount);
-    printf("destroying1\n");/*
     free(r);
-    printf("destroying2\n");
     free(sub_r);
-    printf("destroying3\n");
     free(sub_r_pre);
-    printf("destroying4\n");
-    free(r_pre);/*
-    printf("destroying5\n");
+    free(r_pre);
     free(num_in_links);
-    printf("destroying6\n");
-    free(num_out_links);*/
-    printf("done\n");
+    free(num_out_links);
     
 }
